bc6c05a57faeb79089b51fd4e9f3f11a42b36904
anandhu-as <anandhuas010203@gmail.com>
2026-06-24 00:34:17 +0530

    implement UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,27 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import productsRouter from './routes/products.js';
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import productsRouter from "./routes/products.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/api/products', productsRouter);
+app.use("/api/products", productsRouter);
 
-
-
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
 });
 
-
-app.listen(PORT || 3001, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
